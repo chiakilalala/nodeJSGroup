7f72ada0ff31c80c90dd182088fb6cb92c2c3908
chiakilalala <chiaki205102@gmail.com>
2022-04-03 15:23:39 +0800

    建置環境

---
+++ server.js
@@ -0,0 +1,8 @@
+
+   
+const http = require('http')
+const port = process.env.PORT || 3005
+const serverListener = require('./serverListener')
+const server = http.createServer(serverListener)
+
+server.listen(port)
